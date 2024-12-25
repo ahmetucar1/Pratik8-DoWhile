f741d2ec2cfcef206e97c35c794fdb899c477e79
ahmetucar1 <213214006@stu.adu.edu.tr>
2024-12-25 23:57:43 +0300

    Project is done

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Pratik8
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+           
+         Console.Write("Limit : ");
+         int.TryParse(Console.ReadLine(), out int limit);
+
+         int i = 0;
+         while (i <= limit)
+        {
+         Console.WriteLine("Ben bir Patika'lıyım");
+         i++;
+        }
+        
+ 
+        Console.WriteLine("Limit girin: ");
+        int.TryParse(Console.ReadLine(), out int limit);
+
+        int j = 0;
+        do
+        {
+         Console.WriteLine("Ben bir Patika'lıyım");
+         j++;
+        
+        } while (j <= limit);
+
+        // While döngüsü koşulu baştan kontrol eder, bu yüzden koşul sağlanmazsa hiç çalışmaz.
+        // Do-While döngüsü ise en az bir kez çalışır, sonra koşulu kontrol eder.
+
+
+        }
+    }
+}
+
